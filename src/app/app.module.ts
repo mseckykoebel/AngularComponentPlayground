bc69cd2d01f78020ce433327fac76e626b313e97
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-08-15 18:00:45 -0500

    adding more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
-import { GithubFollowersService } from './services/github-followers.service';
-import { AppErrorHandler } from './common/app-error-handler';
-import { PostService } from './services/post.service';
-import { HttpModule } from '@angular/http';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { SummaryPipe } from './summary.pipe';
-import { AuthorsService } from './authors.service';
-import { CoursesService } from './courses.service';
-import { CoursesComponent } from './courses.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
-import { NgModule, ErrorHandler } from '@angular/core';
+import { GithubFollowersService } from "./services/github-followers.service";
+import { AppErrorHandler } from "./common/app-error-handler";
+import { PostService } from "./services/post.service";
+import { HttpModule } from "@angular/http";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
+import { SummaryPipe } from "./summary.pipe";
+import { AuthorsService } from "./authors.service";
+import { CoursesService } from "./courses.service";
+import { CoursesComponent } from "./courses.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule, ErrorHandler } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
-import { InputFormatDirective } from './input-format.directive';
-import { TitleCasePipe } from './title-case.pipe';
-import { LikeComponent } from './like/like.component';
-import { ZippyComponent } from './zippy/zippy.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { PostsComponent } from './posts/posts.component';
-import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GithubProfileComponent } from './github-profile/github-profile.component';
-import { NotFoundComponent } from './not-found/not-found.component'; 
+import { AppComponent } from "./app.component";
+import { CourseComponent } from "./course/course.component";
+import { AuthorsComponent } from "./authors/authors.component";
+import { FavoriteComponent } from "./favorite/favorite.component";
+import { PanelComponent } from "./panel/panel.component";
+import { InputFormatDirective } from "./input-format.directive";
+import { TitleCasePipe } from "./title-case.pipe";
+import { LikeComponent } from "./like/like.component";
+import { ZippyComponent } from "./zippy/zippy.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { NewCourseFormComponent } from "./new-course-form/new-course-form.component";
+import { ChangePasswordComponent } from "./change-password/change-password.component";
+import { PostsComponent } from "./posts/posts.component";
+import { GithubFollowersComponent } from "./github-followers/github-followers.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { GithubProfileComponent } from "./github-profile/github-profile.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
   declarations: [
@@ -53,13 +53,20 @@ import { NotFoundComponent } from './not-found/not-found.component';
     NavbarComponent,
     HomeComponent,
     GithubProfileComponent,
-    NotFoundComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "followers/:username", component: GithubProfileComponent },
+      { path: "followers", component: GithubFollowersComponent },
+      { path: "posts", component: PostsComponent },
+      { path: "**", component: NotFoundComponent } // catch any URL in the address bar
+    ])
   ],
   providers: [
     PostService,
@@ -70,4 +77,4 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
